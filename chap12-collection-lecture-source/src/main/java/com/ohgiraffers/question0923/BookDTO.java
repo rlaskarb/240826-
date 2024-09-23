bc870625233921b87262937e0e7a453e418a2930
C:\Lecture <https://github.com/rlaskarb/240826-.git>
2024-09-23 18:41:23 +0900

    240923

--- chap12-collection-lecture-source/src/main/java/com/ohgiraffers/question0923/BookDTO.java
+++ chap12-collection-lecture-source/src/main/java/com/ohgiraffers/question0923/BookDTO.java
@@ -11,12 +11,7 @@ public class BookDTO {
     public void BookDTO(){}
 
     //모든 필드를 초기화 하는 새성자
-<<<<<<< HEAD
     public void BookDTO(int category, String title, String author){
-=======
-    public void BookDTO(int bNo,int category, String title, String author){
-        this.bNo = bNo;
->>>>>>> 688b5c53e855e9dd09bc4bf2aa07ffd7a0284014
         this.category = category;
         this.title = title;
         this.author = author;
@@ -24,15 +19,6 @@ public class BookDTO {
     }
 
     // getter / setter
-
-
-<<<<<<< HEAD
-=======
-    public int getbNo() {return bNo;}
-
-    public void setbNo(int bNo) {this.bNo = bNo;}
-
->>>>>>> 688b5c53e855e9dd09bc4bf2aa07ffd7a0284014
     public int getCategory() {return category;}
 
     public void setCategory(int category) {this.category = category;}
@@ -48,7 +34,6 @@ public class BookDTO {
 
     //toString
 
-<<<<<<< HEAD
     @Override
     public String toString() {
         return "BookDTO{" +
@@ -56,17 +41,5 @@ public class BookDTO {
                 ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 '}';
-=======
-
-    @Override
-    public String toString() {
-        return "BookDTO{" +
-                "bNo=" + bNo +
-                ", category=" + category +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-
->>>>>>> 688b5c53e855e9dd09bc4bf2aa07ffd7a0284014
     }
 }
--- chap12-collection-lecture-source/src/main/java/com/ohgiraffers/question0923/BookManager.java
+++ chap12-collection-lecture-source/src/main/java/com/ohgiraffers/question0923/BookManager.java
@@ -7,7 +7,6 @@ public class BookManager {
 
     public BookManager(){
         //해당 BookDTO 객체를 리스트에 담기.
-<<<<<<< HEAD
         BookDTO bookDTO = new BookDTO();
 
         ArrayList<BookDTO> arrayList = new ArrayList<>();
@@ -26,16 +25,6 @@ public class BookManager {
     }
 
     public void searchBook(String btitle){
-=======
-        BookDTO bookDTO1 = new BookDTO();
-    }
-
-    public void deleteBook(int key){
-        //전달받은 위치의 인덱스 값 제거
-    }
-
-    public void searchBook(String title){
->>>>>>> 688b5c53e855e9dd09bc4bf2aa07ffd7a0284014
         //전달받은 제목으로 리스트에서 조회하여 일치하는 객체가 있으면
         // 해당 도서정보를 출력하고, 없는 경우 조회된 도서가 목록에 없습니다. 출력.
     }
